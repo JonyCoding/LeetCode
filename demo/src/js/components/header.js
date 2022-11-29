7d3f67a45b971c87b60bbab404396b440624f9da
Jony <xie.yanpeng@nets-tech.com>
2022-11-29 19:44:52 +0800

    头部响应式‘

--- demo/src/js/components/header.js
+++ demo/src/js/components/header.js
@@ -1,45 +1,46 @@
-
 Vue.component("h-header", {
-    data: function () {
-        return {};
-    },
-    methods: {},
-    template: `                
+	data: function () {
+		return {
+			isSelect: "literatureService",
+		};
+	},
+	methods: {},
+	template: `                
   <header class="index-head">
 	<div class="flex-container">
 		<div class="logo">
 			<img src="./img/logo.png" alt="" srcset="" />
 		</div>
 		<div class="head-menu">
+            <i class="close-action el-icon-close"></i>
+            <div id="mobil-logo" class="user-action">
+                <img src="./img/logo-blue.png" alt="" srcset="" />
+            </div>
+			<ul class="menu-list">
+				<li :id="isSelect==='index'?'is-select':''" class="menu-item"><a href="">首页</a></li>
+				<li :id="isSelect==='energyData'?'is-select':''" class="menu-item"><a href="">能源数据</a></li>
+				<li :id="isSelect==='ourResearch'?'is-select':''" class="menu-item"><a href="">我们的研究</a></li>
+				<li :id="['literatureService','patentService','newsPolicy'].includes(isSelect)?'is-select':''" class="parent-menu menu-item">
+					<a class="child-menu-title" href="javascript:;">知识服务<i class="bi bi-chevron-down bi-active"></i></a>
+					<ul id="show-knowledge" class="child-menu-list">
+						<li :id="isSelect==='literatureService'?'is-select-child':''"><a>文献服务</a></li>
+						<li :id="isSelect==='patentService'?'is-select-child':''"><a>专利服务</a></li>
+						<li :id="isSelect==='newsPolicy'?'is-select-child':''"><a>新闻/政策</a></li>
+					</ul>
+				</li>
+				<li :id="isSelect==='dataTool'?'is-select':''" class="menu-item"><a href="">数据/工具</a></li>
+				<li :id="['researchCooperation','jobOpportunities'].includes(isSelect)?'is-select':''" class="parent-menu menu-item">
+					<a class="child-menu-title" href="javascript:;">联系我们<i class="bi bi-chevron-down bi-active"></i></a>
+					<ul id="show-technology" class="child-menu-list">
+						<li :id="isSelect==='researchCooperation'?'is-select-child':''"><a>研究合作</a></li>
+						<li :id="isSelect==='jobOpportunities'?'is-select-child':''"><a>工作机会</a></li>
+					</ul>
+				</li>
+			</ul>
 			<div id="user-action-mobil" class="user-action">
 				<div class="login-button login"><a href="">登录</a></div>
 				<div class="login-button register"><a href="">注册</a></div>
 			</div>
-			<ul class="menu-list">
-				<li class="menu-item"><a href="">首页</a></li>
-				<li class="menu-item"><a href="">能源数据</a></li>
-				<li id="knowledge" class="parent-menu menu-item">
-					<a href="javascript:;">知识服务<i class="bi bi-chevron-down bi-active"></i></a>
-					<ul id="show-knowledge" class="child-menu-list">
-						<li><a>能源新闻</a></li>
-						<li><a>能源政策</a></li>
-						<li><a>政策解读</a></li>
-						<li><a>碳中和解读</a></li>
-						<li><a>能源讲座</a></li>
-						<li><a>碳中和传递</a></li>
-					</ul>
-				</li>
-				<li class="menu-item"><a href="">专利导航</a></li>
-				<li id="technology" class="parent-menu menu-item">
-					<a href="javascript:;">技术评估<i class="bi bi-chevron-down bi-active"></i></a>
-					<ul id="show-technology" class="child-menu-list">
-						<li><a>技术清单</a></li>
-						<li><a>能源科技评价</a></li>
-					</ul>
-				</li>
-				<li class="menu-item"><a href="">工具</a></li>
-				<li class="menu-item"><a href="">关于我们</a></li>
-			</ul>
 		</div>
 		<div id="user-action-pc" class="user-action">
 			<div class="login-button login"><a href="">登录</a></div>
@@ -51,6 +52,7 @@ Vue.component("h-header", {
 			</div>
 		</div>
 	</div>
+    <div class="diskDuck"></div>
 </header>
 
 `,
@@ -58,43 +60,44 @@ Vue.component("h-header", {
 
 //菜单栏
 $(document).ready(function () {
-    var i = 1;
-    $("#menu-action").click(function () {
-        if (i == 0) {
-            // 隐藏
-            $("#menu-action").addClass("user-menu-close");
-            $(".head-menu").removeClass("head-active");
-            $("#menu-action").removeClass("user-menu-action");
-            $(".diskDuck").removeClass("disk-show");
-            i = 1;
-        } else {
-            // 显示
-            $("#menu-action").addClass("user-menu-action");
-            $(".head-menu").addClass("head-active");
-            $("#menu-action").removeClass("user-menu-close");
-            $(".diskDuck").addClass("disk-show");
-            i = 0;
-        }
-    });
-    $(".diskDuck").click(function () {
-        // 隐藏
-        $("#menu-action").addClass("user-menu-close");
-        $(".head-menu").removeClass("head-active");
-        $("#menu-action").removeClass("user-menu-action");
-        $(".diskDuck").removeClass("disk-show");
-        i = 1;
-    });
-    $(".logp").click(function () {
-        if (i == 0) {
-            $("#menu-action").addClass("user-menu-close");
-            $(".index-head").removeClass("head-active");
-            $("#menu-action").removeClass("user-menu-action");
-            i = 1;
-        } else {
-            $("#menu-action").addClass("user-menu-action");
-            $(".index-head").addClass("head-active");
-            $("#menu-action").removeClass("user-menu-close");
-            i = 0;
-        }
-    });
+	var i = 1;
+	$("#menu-action").click(function () {
+		if (i == 0) {
+			// 隐藏
+			$("#menu-action").addClass("user-menu-close");
+			$(".head-menu").removeClass("head-active");
+			$("#menu-action").removeClass("user-menu-action");
+			$(".diskDuck").removeClass("disk-show");
+
+			i = 1;
+		} else {
+			// 显示
+			$("#menu-action").addClass("user-menu-action");
+			$(".head-menu").addClass("head-active");
+			$("#menu-action").removeClass("user-menu-close");
+			$(".diskDuck").addClass("disk-show");
+			i = 0;
+		}
+	});
+	$(".diskDuck,.close-action").click(function () {
+		// 隐藏
+		$("#menu-action").addClass("user-menu-close");
+		$(".head-menu").removeClass("head-active");
+		$("#menu-action").removeClass("user-menu-action");
+		$(".diskDuck").removeClass("disk-show");
+		i = 1;
+	});
+	$(function () {
+		$(".menu-list li").click(function () {
+			if ($(this).hasClass('menu-item-active')) {
+				$(".menu-list li").removeClass("menu-item-active");
+			} else {
+				$(".menu-list li").removeClass("menu-item-active");
+				$(this).addClass("menu-item-active"); // 添加当前元素的样式
+			}
+		});
+        $(".menu-list li").mouseleave(function () {
+				$(".menu-list li").removeClass("menu-item-active");
+		});
+	});
 });
