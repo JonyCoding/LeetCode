938d8e2c00521593a4649978c76bffa352354ae7
Jony <xie.yanpeng@nets-tech.com>
2023-06-09 10:53:02 +0800

    fix-UI修正

--- demo/plumb/data.js
+++ demo/plumb/data.js
@@ -1,75 +1,80 @@
 // 连接关系数据
 var connectionDataDefault = [
-    { "source": "node11", "target": "node14", "sourceAnchor": "Right", "targetAnchor": "Left" },
-    { "source": "node11", "target": "node15", "sourceAnchor": "Top", "targetAnchor": "Left" },
-    { "source": "node15", "target": "node13", "sourceAnchor": "Right", "targetAnchor": "Left" },
-    { "source": "node15", "target": "node12", "sourceAnchor": "Top", "targetAnchor": "Left" },
-    { "source": "node14", "target": "node16", "sourceAnchor": "Right", "targetAnchor": "Left" },
-    { "source": "node13", "target": "node16", "sourceAnchor": "Right", "targetAnchor": "Top" }];
+	{
+		source: "node14",
+		target: "node16",
+		sourceAnchor: "Right",
+		targetAnchor: "Left",
+		sourceNodeType: "node",
+		targetNodeType: "node",
+	},
+	{
+		source: "node11",
+		target: "node14",
+		sourceAnchor: "Right",
+		targetAnchor: "Left",
+		sourceNodeType: "node",
+		targetNodeType: "node",
+	},
+	{
+		source: "node11",
+		target: "node15",
+		sourceAnchor: "Top",
+		targetAnchor: "Left",
+		sourceNodeType: "node",
+		targetNodeType: "empty",
+	},
+	{
+		source: "node15",
+		target: "node13",
+		sourceAnchor: "Right",
+		targetAnchor: "Left",
+		sourceNodeType: "empty",
+		targetNodeType: "node",
+	},
+	{
+		source: "node15",
+		target: "node12",
+		sourceAnchor: "Top",
+		targetAnchor: "Left",
+		sourceNodeType: "empty",
+		targetNodeType: "node",
+	},
+	{
+		source: "node1686230540818",
+		target: "node1686230474807",
+		sourceAnchor: "Right",
+		targetAnchor: "Left",
+		sourceNodeType: "node",
+		targetNodeType: "node",
+	},
+	{
+		source: "node13",
+		target: "node17",
+		sourceAnchor: "Right",
+		targetAnchor: "Left",
+		sourceNodeType: "node",
+		targetNodeType: "explain",
+	},
+	{
+		source: "node17",
+		target: "node16",
+		sourceAnchor: "Right",
+		targetAnchor: "Top",
+		sourceNodeType: "explain",
+		targetNodeType: "node",
+	},
+];
 // 流程图数据
 var flowchartDataDefault = [
-    {
-      "source": "node14",
-      "target": "node16",
-      "sourceAnchor": "Right",
-      "targetAnchor": "Left",
-      "sourceNodeType": "node",
-      "targetNodeType": "node"
-    },
-    {
-      "source": "node11",
-      "target": "node14",
-      "sourceAnchor": "Right",
-      "targetAnchor": "Left",
-      "sourceNodeType": "node",
-      "targetNodeType": "node"
-    },
-    {
-      "source": "node11",
-      "target": "node15",
-      "sourceAnchor": "Top",
-      "targetAnchor": "Left",
-      "sourceNodeType": "node",
-      "targetNodeType": "empty"
-    },
-    {
-      "source": "node15",
-      "target": "node13",
-      "sourceAnchor": "Right",
-      "targetAnchor": "Left",
-      "sourceNodeType": "empty",
-      "targetNodeType": "node"
-    },
-    {
-      "source": "node15",
-      "target": "node12",
-      "sourceAnchor": "Top",
-      "targetAnchor": "Left",
-      "sourceNodeType": "empty",
-      "targetNodeType": "node"
-    },
-    {
-      "source": "node1686230540818",
-      "target": "node1686230474807",
-      "sourceAnchor": "Right",
-      "targetAnchor": "Left",
-      "sourceNodeType": "node",
-      "targetNodeType": "node"
-    },
-    {
-      "source": "node13",
-      "target": "node17",
-      "sourceAnchor": "Right",
-      "targetAnchor": "Left",
-      "sourceNodeType": "node",
-      "targetNodeType": "explain"
-    },
-    {
-      "source": "node17",
-      "target": "node16",
-      "sourceAnchor": "Right",
-      "targetAnchor": "Top",
-      "sourceNodeType": "explain",
-      "targetNodeType": "node"
-    }
-  ];
+	{ id: "node11", top: 240, left: 101, label: "馏分油", type: "node" },
+	{ id: "node12", top: 40, left: 338, label: "石脑油", type: "node" },
+	{ id: "node13", top: 133, left: 339, label: "尾油", type: "node" },
+	{ id: "node14", top: 240, left: 339, label: "润滑油", type: "node" },
+	{ id: "node15", top: 131, left: 213, label: "", type: "empty" },
+	{ id: "node16", top: 240, left: 601, label: "润滑油基础油", type: "node" },
+	{ id: "node17", top: 144, left: 499, label: "126", type: "explain" },
+	{ id: "node18", top: 173, left: 415, label: "14", type: "remark" },
+	{ id: "node19", top: 250, left: 506, label: "14", type: "explain" },
+	{ id: "node20", top: 261, left: 184, label: "13溶剂脱蜡及精致", type: "remark" },
+];
--- demo/plumb/index.js
+++ demo/plumb/index.js
@@ -23,42 +23,42 @@ $("#exportBtn").click(function () {
 
 // [添加节点] 按钮点击事件处理程序
 $("#addNodeBtn").click(function () {
-	addNode(true, 'node')
+	addNode(true, "node");
 });
 // [添加中继] 按钮点击事件处理程序
 $("#addEmptyBtn").click(function () {
-	addNode(false, 'empty')
+	addNode(false, "empty");
 });
 // [添加连线说明文字] 按钮点击事件处理程序
 $("#addExplainBtn").click(function () {
-	addNode(true, 'explain')
+	addNode(true, "explain");
 });
 // [添加图icon] 按钮点击事件处理程序
 $("#addIconBtn").click(function () {
-	addNode(true, 'icon')
+	addNode(true, "icon");
 });
 // [添加备注] 按钮点击事件处理程序
 $("#addRemarkBtn").click(function () {
-	addNode(true, 'remark')
+	addNode(true, "remark");
 });
 // 预览模式按钮点击事件处理程序
 $("#reviewBtn").click(function () {
-	isReview = !isReview
-	$('#canvas').toggleClass('review');
+	isReview = !isReview;
+	$("#canvas").toggleClass("review");
 	setTimeout(() => {
-		reload()
+		reload();
 	}, 1);
 });
 
 // 增加节点的共同方法
 function addNode(requireInput, nodeType) {
-	const newNodeId = "node" + Date.now();;
-	let newNodeLabel = ""
-	let newNodeColor = "red"
+	const newNodeId = "node" + Date.now();
+	let newNodeLabel = "";
+	let newNodeColor = "red";
 
 	if (!nodeTypeList.includes(nodeType)) {
 		alert("类型不合法");
-		return
+		return;
 	}
 	// 确定是否需要输入label标签
 	if (requireInput) {
@@ -66,14 +66,14 @@ function addNode(requireInput, nodeType) {
 		newNodeLabel = prompt("请输入节点的标签：", "Node " + (flowchartData.length + 1));
 		newNodeColor = prompt("请输入节点的颜色色号，默认是红色：", "red");
 		if (newNodeColor === null) {
-			console.log("")
+			console.log("");
 		}
 		if (newNodeLabel === null) {
 			// 用户点击了取消按钮，停止添加节点
 			return;
 		}
 	} else {
-		newNodeLabel = nodeType === 'empty' ? '' : "Node " + (flowchartData.length + 1)
+		newNodeLabel = nodeType === "empty" ? "" : "Node " + (flowchartData.length + 1);
 	}
 
 	var newNode = {
@@ -86,91 +86,99 @@ function addNode(requireInput, nodeType) {
 	// 添加新节点到数据列表
 	flowchartData.push(newNode);
 	// 渲染出来
-	addElement(newNode)
+	addElement(newNode);
 
 	// 更新数据
 	window.localStorage.setItem("flowchartData", JSON.stringify(flowchartData));
 }
 
-
 // 渲染节点
 function addElement(newNode) {
 	// 判断是否有拖拽事件发生
 	let isDragged = false;
+	// 双击事件延迟
+	const DELAY = 260;
+	// 记录点击了几次
+	let clicks = 0;
+	// 单击事件防抖定时器
+	let timer = null;
 	// 创建新节点的 DOM 元素并添加到画布
-	console.log("newNode")
 	var newElement = $("<div>")
 		.attr("id", newNode.id)
 		.attr("node-type", newNode.type)
 		.addClass(newNode.type)
-		.addClass('node-item')
+		.addClass("node-item")
 		.css({
 			top: newNode.top + "px",
 			left: newNode.left + "px",
 		})
 		.text(newNode.label)
-		.on('mousedown', function (event) {
+		.on("mousedown", function () {
 			isDragged = false;
 		})
-		.on('mousemove', function (event) {
+		.on("mousemove", function () {
 			isDragged = true;
 		})
-		.on('mouseup', function (event) {
+		.on("mouseup", function () {
 			if (!isDragged) {
-				const id = $(this).attr("id")
-				const node = $(this).attr("node-type")
-				// 预览模式时才触发单击事件
-				isReview ? nodeClick(id, node) : ""
+				clicks++; // 计数器
+				if (clicks === 1) {
+					timer = setTimeout(() => {
+						const id = $(this).attr("id");
+						const node = $(this).attr("node-type");
+						// 预览模式时才触发单击事件
+						isReview ? nodeClick(id, node) : "";
+						// 重置计数器
+						clicks = 0;
+					}, DELAY);
+				} else {
+					// 双击事件清除定时器，防止出发单击事件
+					clearTimeout(timer);
+					// 重置计数器
+					clicks = 0;
+				}
 			}
 		});
-		if(isReview && newNode.type === 'empty'){
-			newElement.css({
-				'top': (newNode.top + 23) + 'px',
-				'left': (newNode.left + 21) + 'px'
-			});
-		}
-	$("#canvas").append(newElement);
-	
+	if (isReview && newNode.type === "empty") {
+		newElement.css({
+			top: newNode.top + 22 + "px",
+			left: newNode.left + 22 + "px",
+		});
+	}
 
+	console.log("newElement", newElement);
+	$("#canvas").append(newElement);
 
 	// 编辑模式双击为删除事件，预览模式为自定义事件
 	if (!isReview) {
 		// 绑定双击删除事件
-		delNode(newElement[0])
-	}else{
-		newElement[0].addEventListener('dblclick', function () {
-			const id = $(this).attr("id")
-			const node = $(this).attr("node-type")
-			nodeClick(id, node)
-		});
+		handledelNode(newElement[0]);
+	} else {
+		// 绑定双击自定义事件
+		handleDblDelNode(newElement[0])
 	}
 	// 可拖拽
 	if (!isReview) {
 		instance.draggable(newElement, {
 			containment: "parent",
 			drag: function (event) {
-				changeCanvas(event)
-			}
+				changeCanvas(event);
+			},
 		});
 	}
 
 	// 让节点可以自由创建连线
 	if (connectableList.includes(newNode.type)) {
-		const paintStyle = isReview ? reviewPaintSettings : paintSettings
+		const paintStyle = isReview ? reviewPaintSettings : paintSettings;
 		// 使用 addEndpoints 函数添加端点
-		instance.addEndpoints(
-			newNode.id,
-			endpointOptions,
-			paintStyle
-		);
+		instance.addEndpoints(newNode.id, endpointOptions, paintStyle);
 	}
 }
-
 
 // [导出数据]按钮点击后处理函数
 function exportBtnHandle() {
 	// 获取所有的节点
-	const nodeStr = '.' + nodeTypeList.join(',.')
+	const nodeStr = "." + nodeTypeList.join(",.");
 	var nodes = $(nodeStr);
 	var flowchartData = getFkowCharData(nodes);
 
@@ -213,14 +221,13 @@ function getConnectionData(connections) {
 		const sourceNodeType = $("#" + sourceId).attr("node-type");
 		const targetNodeType = $("#" + targetId).attr("node-type");
 
-
 		connectionData.push({
 			source: sourceId,
 			target: targetId,
 			sourceAnchor: sourceAnchorType,
 			targetAnchor: targetAnchorType,
 			sourceNodeType: sourceNodeType,
-			targetNodeType: targetNodeType
+			targetNodeType: targetNodeType,
 		});
 	});
 	return connectionData;
@@ -230,30 +237,23 @@ function getConnectionData(connections) {
 function initNode(flowchartData) {
 	// 遍历创建节点
 	flowchartData.forEach(function (node) {
-		addElement(node)
+		addElement(node);
 	});
 }
 
 // 创建连线
 function createConnection(connectionData) {
-	console.log("connectionData", connectionData)
-	console.log("connectorEmptyStyleList", connectorEmptyStyleList)
-
 	// 根据起始节点和结束节点类型选择不同的样式
 	connectionData.forEach(function (connection) {
-		let connectorStyle = connectorStyleDefault
+		let connectorStyle = connectorStyleDefault;
 		// 如果是特殊节点，则加载特殊样式
 		if (connectorEmptyStyleList.includes(connection.sourceNodeType)) {
-			console.log("sourceNodeType")
-			connectorStyle = connectorStyleSource
+			connectorStyle = connectorStyleSource;
 		}
 
 		if (connectorEmptyStyleList.includes(connection.targetNodeType)) {
-			console.log("targetNodeType")
-			connectorStyle = connectorStyleTarget
-
+			connectorStyle = connectorStyleTarget;
 		}
-		console.log("connectorStyle", connectorStyle)
 		instance.connect({
 			source: connection.source,
 			target: connection.target,
@@ -275,27 +275,33 @@ function connectionDoubleClick() {
 // 给所有节点双击删除节点事件
 function nodeDoubleClick() {
 	// 使用你的节点类名替换'.node-class'
-	var nodes = document.querySelectorAll('.node-item');
+	var nodes = document.querySelectorAll(".node-item");
 	nodes.forEach(function (node) {
-		delNode(node)
+		handledelNode(node);
 	});
 }
 // 删除节点操作
-function delNode(node) {
-	node.addEventListener('dblclick', function () {
+function handledelNode(newElement) {
+	newElement.addEventListener("dblclick", function () {
 		if (confirm("确定要删除这个节点吗?")) {
 			// 删除节点的jsPlumb逻辑
-			instance.remove(node);
-			// 给出提示
-			// alert('节点已删除');
+			instance.remove(newElement);
 		}
+	});
+}
+// 绑定双击事件
+function handleDblDelNode(newElement) {
+	newElement.addEventListener("dblclick", function () {
+		const id = $(this).attr("id");
+		const node = $(this).attr("node-type");
+		nodeDblClick(id, node);
 	});
 }
 
 // 重新渲染
 function reload() {
-	instance.deleteEveryEndpoint();  // 删除所有端点和连线
-	$('#canvas').empty();  // 删除所有节点
+	instance.deleteEveryEndpoint(); // 删除所有端点和连线
+	$("#canvas").empty(); // 删除所有节点
 	// 根据数据创建节点
 	initNode(flowchartData);
 	// 根据数据创建连线
@@ -305,7 +311,7 @@ function reload() {
 // 拖拽节点变化画布宽度
 function changeCanvas(event) {
 	var element = $(event.el);
-	var canvas = document.getElementById('canvas');
+	var canvas = document.getElementById("canvas");
 
 	var canvasWidth = canvas.offsetWidth;
 	var canvasHeight = canvas.offsetHeight;
@@ -316,33 +322,32 @@ function changeCanvas(event) {
 	var expansion = 300; // 扩展的距离
 	if (canvasWidth - elementPosition.left - width < buffer) {
 		// 如果元素接近右边缘
-		canvas.style.width = (canvasWidth + expansion) + 'px';
+		canvas.style.width = canvasWidth + expansion + "px";
 	}
 
 	if (canvasHeight - elementPosition.top < buffer) {
 		// 如果元素接近底边缘
-		canvas.style.height = (canvasHeight + expansion) + 'px';
+		canvas.style.height = canvasHeight + expansion + "px";
 	}
-
 }
 
-// 计算节点的偏移量 
-function nodeOffset(newNode,newElement){
+// 计算节点的偏移量
+function nodeOffset(newNode, newElement) {
 	// 获取元素的宽度和高度
 	var elementWidth = newElement.width();
 	var elementHeight = newElement.height();
 	newElement.css({
-		'top': (newNode.top + elementHeight / 2) + 'px',
-		'left': (newNode.left + elementWidth / 2) + 'px'
+		top: newNode.top + elementHeight / 2 + "px",
+		left: newNode.left + elementWidth / 2 + "px",
 	});
-	return newElement
+	return newElement;
 }
 
 // 点击节点的公共处理函数
 function nodeClick(id, node) {
-	alert('单机事件：id:' + id + ' - ' + 'node:' + node)
+	alert("单机事件：id:" + id + " - " + "node:" + node);
 }
 // 双击节点的公共处理函数
 function nodeDblClick(id, node) {
-		alert('双击事件：id:' + id + ' - ' + 'node:' + node)
+	alert("双击事件：id:" + id + " - " + "node:" + node);
 }
