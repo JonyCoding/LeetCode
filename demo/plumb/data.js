a7c3b78d0dab95a8cf992ba3a9b779b8136c848f
Jony <1070474647@qq.com>
2023-06-11 14:20:10 +0800

    feat-画布

--- demo/plumb/data.js
+++ demo/plumb/data.js
@@ -1,158 +1,174 @@
 // 连接关系数据
 var connectionDataDefault = [
-	{
-		source: "node14",
-		target: "node16",
-		sourceAnchor: "Right",
-		targetAnchor: "Left",
-		sourceNodeType: "node",
-		targetNodeType: "node",
-	},
-	{
-		source: "node11",
-		target: "node14",
-		sourceAnchor: "Right",
-		targetAnchor: "Left",
-		sourceNodeType: "node",
-		targetNodeType: "node",
-	},
-	{
-		source: "node11",
-		target: "node15",
-		sourceAnchor: "Top",
-		targetAnchor: "Left",
-		sourceNodeType: "node",
-		targetNodeType: "empty",
-	},
-	{
-		source: "node15",
-		target: "node13",
-		sourceAnchor: "Right",
-		targetAnchor: "Left",
-		sourceNodeType: "empty",
-		targetNodeType: "node",
-	},
-	{
-		source: "node15",
-		target: "node12",
-		sourceAnchor: "Top",
-		targetAnchor: "Left",
-		sourceNodeType: "empty",
-		targetNodeType: "node",
-	},
-	{
-		source: "node13",
-		target: "node17",
-		sourceAnchor: "Right",
-		targetAnchor: "Left",
-		sourceNodeType: "node",
-		targetNodeType: "explain",
-	},
-	{
-		source: "node17",
-		target: "node16",
-		sourceAnchor: "Right",
-		targetAnchor: "Top",
-		sourceNodeType: "explain",
-		targetNodeType: "node",
-	},
-	{
-		source: "node17",
-		target: "node1686277149257",
-		sourceAnchor: "Top",
-		targetAnchor: "Left",
-		sourceNodeType: "explain",
-		targetNodeType: "node",
-	},
-];
+    {
+        "source": "node14",
+        "target": "node16",
+        "sourceAnchor": "Right",
+        "targetAnchor": "Left",
+        "sourceNodeType": "node",
+        "targetNodeType": "node"
+    },
+    {
+        "source": "node11",
+        "target": "node14",
+        "sourceAnchor": "Right",
+        "targetAnchor": "Left",
+        "sourceNodeType": "node",
+        "targetNodeType": "node"
+    },
+    {
+        "source": "node11",
+        "target": "node15",
+        "sourceAnchor": "Top",
+        "targetAnchor": "Left",
+        "sourceNodeType": "node",
+        "targetNodeType": "empty"
+    },
+    {
+        "source": "node15",
+        "target": "node13",
+        "sourceAnchor": "Right",
+        "targetAnchor": "Left",
+        "sourceNodeType": "empty",
+        "targetNodeType": "node"
+    },
+    {
+        "source": "node15",
+        "target": "node12",
+        "sourceAnchor": "Top",
+        "targetAnchor": "Left",
+        "sourceNodeType": "empty",
+        "targetNodeType": "node"
+    },
+    {
+        "source": "node13",
+        "target": "node17",
+        "sourceAnchor": "Right",
+        "targetAnchor": "Left",
+        "sourceNodeType": "node",
+        "targetNodeType": "explain"
+    },
+    {
+        "source": "node17",
+        "target": "node16",
+        "sourceAnchor": "Right",
+        "targetAnchor": "Top",
+        "sourceNodeType": "explain",
+        "targetNodeType": "node"
+    },
+    {
+        "source": "node17",
+        "target": "node1686277149257",
+        "sourceAnchor": "Top",
+        "targetAnchor": "Left",
+        "sourceNodeType": "explain",
+        "targetNodeType": "node"
+    },
+    {
+        "source": "node16",
+        "target": "node1686463857934",
+        "sourceAnchor": "Right",
+        "targetAnchor": "Left",
+        "sourceNodeType": "node",
+        "targetNodeType": "node"
+    }
+]
 // 流程图数据
 var flowchartDataDefault = [
-	{
-		id: "node11",
-		top: 240,
-		left: 101,
-		label: "馏分油",
-		type: "node",
-		typeClass: "",
-	},
-	{
-		id: "node12",
-		top: 40,
-		left: 338,
-		label: "石脑油",
-		type: "node",
-		typeClass: "",
-	},
-	{
-		id: "node13",
-		top: 131,
-		left: 341,
-		label: "尾油",
-		type: "node",
-		typeClass: "",
-	},
-	{
-		id: "node14",
-		top: 240,
-		left: 339,
-		label: "润滑油",
-		type: "node",
-		typeClass: "",
-	},
-	{
-		id: "node15",
-		top: 131,
-		left: 211,
-		label: "",
-		type: "empty",
-		typeClass: "",
-	},
-	{
-		id: "node16",
-		top: 240,
-		left: 601,
-		label: "润滑油基础油",
-		type: "node",
-		typeClass: "",
-	},
-	{
-		id: "node17",
-		top: 141,
-		left: 521,
-		label: "126",
-		type: "explain",
-		typeClass: "",
-	},
-	{
-		id: "node18",
-		top: 173,
-		left: 415,
-		label: "14",
-		type: "remark",
-		typeClass: "",
-	},
-	{
-		id: "node19",
-		top: 250,
-		left: 506,
-		label: "14",
-		type: "explain",
-		typeClass: "",
-	},
-	{
-		id: "node20",
-		top: 261,
-		left: 184,
-		label: "13溶剂脱蜡及精致",
-		type: "remark",
-		typeClass: "",
-	},
-	{
-		id: "node1686277149257",
-		top: 54,
-		left: 609,
-		label: "加一个",
-		type: "node",
-		typeClass: "",
-	},
-];
+    {
+        "id": "node11",
+        "top": 240,
+        "left": 101,
+        "label": "馏分油",
+        "type": "node",
+        "typeClass": ""
+    },
+    {
+        "id": "node12",
+        "top": 40,
+        "left": 338,
+        "label": "石脑油",
+        "type": "node",
+        "typeClass": ""
+    },
+    {
+        "id": "node13",
+        "top": 131,
+        "left": 341,
+        "label": "尾油",
+        "type": "node",
+        "typeClass": ""
+    },
+    {
+        "id": "node14",
+        "top": 240,
+        "left": 339,
+        "label": "润滑油",
+        "type": "node",
+        "typeClass": ""
+    },
+    {
+        "id": "node15",
+        "top": 131,
+        "left": 211,
+        "label": "",
+        "type": "empty",
+        "typeClass": ""
+    },
+    {
+        "id": "node16",
+        "top": 240,
+        "left": 601,
+        "label": "润滑油基础油",
+        "type": "node",
+        "typeClass": ""
+    },
+    {
+        "id": "node17",
+        "top": 141,
+        "left": 521,
+        "label": "126",
+        "type": "explain",
+        "typeClass": ""
+    },
+    {
+        "id": "node18",
+        "top": 173,
+        "left": 415,
+        "label": "14",
+        "type": "remark",
+        "typeClass": ""
+    },
+    {
+        "id": "node19",
+        "top": 250,
+        "left": 506,
+        "label": "14",
+        "type": "explain",
+        "typeClass": ""
+    },
+    {
+        "id": "node20",
+        "top": 261,
+        "left": 184,
+        "label": "13溶剂脱蜡及精致",
+        "type": "remark",
+        "typeClass": ""
+    },
+    {
+        "id": "node1686277149257",
+        "top": 54,
+        "left": 609,
+        "label": "加一个",
+        "type": "node",
+        "typeClass": ""
+    },
+    {
+        "id": "node1686463857934",
+        "top": 240,
+        "left": 920,
+        "label": "node",
+        "type": "node",
+        "typeClass": "testclass"
+    }
+]
